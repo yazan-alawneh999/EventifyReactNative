a6d111d658e061abd5e347dc1482528d8cc44722
yazan-alawneh999 <yazanalawneh.999@gmail.com>
2025-04-17 05:13:01 +0300

    login

---
+++ utils/Storage.js
@@ -0,0 +1,27 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const storeCredential = async value => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem('credential', jsonValue);
+  } catch (e) {
+    console.error('Error storing data', e);
+  }
+};
+
+export const getCredential = async () => {
+    try {
+        const jsonValue = await AsyncStorage.getItem('credential');
+        return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch (e) {
+        console.error('Error reading data', e);
+        return null;
+    }
+};
+export const logout = async () => {
+    try {
+        await AsyncStorage.removeItem('credential');
+    } catch (e) {
+        console.error('Error removing data', e);
+    }
+};
