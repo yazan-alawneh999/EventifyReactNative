d7101544703c68abfdbf66c383e28ea0f6ed820b
ABIAlzoubi <anas.basam.525@gmail.com>
2025-04-23 10:29:07 +0300

    merge

--- src/screens/Api.tsx
+++ src/screens/Api.tsx
@@ -20,7 +20,7 @@ import {RootStackParamList} from "../navigation/RootStackParamList.ts";
 
 
 
-export const BASE_URL = "https://19c5-149-200-133-207.ngrok-free.app";
+export const BASE_URL = "https://f889-109-107-227-240.ngrok-free.app";
 
 export  const navigationRef = createNavigationContainerRef<RootStackParamList>();
 
